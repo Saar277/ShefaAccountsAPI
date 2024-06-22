ad551c5a07c565caf717720970b3b501f49df98a
Saar277 <saar277@gmail.com>
2024-06-22 21:49:51 +0300

    .

--- src/env.ts
+++ src/env.ts
@@ -26,7 +26,7 @@ export const accountsInfo = [
   },
   {
     NAME: "test",
-    API_KEY: "PKWFGBOOTLH6DI9TBIMA",
-    API_SECRET: "ezClONTh9zvbQg8SDYsNXiL3d8QWah06Z01JVvSM",
+    API_KEY: "PKI9U8OVP89R1AEI8EE1",
+    API_SECRET: "gMOxxKjKEYRrGV9MfrnR7MhwfwqBuI7w0ZeWO7Vj",
   }
 ];
